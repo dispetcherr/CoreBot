665cf6ead609d48ad055b839b36e860d10963a39
dispetcherr <kozmenkovlad9@mail.ru>
2026-03-31 17:35:28 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,9 +7,9 @@ import re
 from urllib.parse import urlparse, parse_qs
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.filters import Command, Text
+from aiogram.filters import Command
+from aiogram.filters.text import Text
 from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.storage.memory import MemoryStorage
 
 TOKEN = os.getenv("BOT_TOKEN")
